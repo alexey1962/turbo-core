697ed938adc3790af9aa85efeb9e0795bb8bb310
Alexey <a.kashutin.dev@gmail.com>
2023-03-05 21:27:57 +0600

    add: popups

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as flsFunctions from "./modules/functions.js"
 
 flsFunctions.isWebp()
+flsFunctions.initPopups()
+flsFunctions.initForm()
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
